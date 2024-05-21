529c1ebea0bc90485a9aa3e10cb5db904c11c9c5
Ana Rodrigues <rodriguesanajulia000@gmail.com>
2024-05-20 23:50:29 -0300

    adjust

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ricky_and_Morty_API'
+  base: '/Ricky_and_Morty_API'
 })
